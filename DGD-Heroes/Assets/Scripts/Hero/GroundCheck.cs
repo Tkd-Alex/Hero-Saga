8499a7be09e8db657a1a244d4db69ce4e357a102
Alessandro Maggio <alex.tkd.alex@gmail.com>
2020-04-10 00:03:32 +0200

    PlayerController, Left, Right, Jump. RigidBody and groud check

---
+++ DGD-Heroes/Assets/Scripts/Hero/GroundCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+	GameObject Player;
+	// Use this for initialization
+	void Start () {
+		Player = gameObject.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		if (collision.collider.tag == "Ground") {
+			Player.GetComponent<PlayerController> ().isGrounded = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D collision){
+		if (collision.collider.tag == "Ground") {
+			Player.GetComponent<PlayerController> ().isGrounded = false;
+		}
+	}
+
+}
---
+++ DGD-Heroes/Assets/Scripts/Hero/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField] float moveSpeed = 2f;
+	[SerializeField] float jumpHeight = 6f;
+
+	public bool isGrounded = false;
+	Rigidbody2D currentRigidBody;
+
+	void Start () {
+		currentRigidBody = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate(){
+		if(Input.GetKey("d") || Input.GetKey("right")){
+			gameObject.GetComponent<SpriteRenderer> ().flipX = false;
+			gameObject.GetComponent<Animator> ().Play ("Owlet_Monster_Run");
+			currentRigidBody.velocity = new Vector2 (moveSpeed, currentRigidBody.velocity.y);
+		} 
+		else if(Input.GetKey("a") || Input.GetKey("left")){
+			gameObject.GetComponent<SpriteRenderer> ().flipX = true;
+			gameObject.GetComponent<Animator> ().Play ("Owlet_Monster_Run");
+			currentRigidBody.velocity = new Vector2 (-moveSpeed, currentRigidBody.velocity.y);
+		}
+		else if(isGrounded == false) gameObject.GetComponent<Animator> ().Play ("Owlet_Monster_Idle");
+
+		if((Input.GetKey("space") || Input.GetKey("w")) && isGrounded == true){
+			currentRigidBody.velocity = new Vector2 (currentRigidBody.velocity.x, jumpHeight);
+			gameObject.GetComponent<Animator> ().Play ("Owlet_Monster_Jump");
+		}
+	}
+
+}
