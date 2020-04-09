4a451f7538f1f12df902ba2add2d40da5a4d49fa
Alessandro Maggio <alex.tkd.alex@gmail.com>
2020-04-09 19:27:13 +0200

    Parallax background

---
+++ DGD-Heroes/Assets/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+	private float length, starpos;
+	[SerializeField] public GameObject cam;
+	[SerializeField] public float parallaxEffect;
+
+	void Start () {
+		starpos = transform.position.x;
+		length = GetComponent<SpriteRenderer> ().bounds.size.x;
+	}
+	
+	void FixedUpdate () {
+		float temp = (cam.transform.position.x * (1 - parallaxEffect));
+		float dist = (cam.transform.position.x * parallaxEffect);
+		transform.position = new Vector3 (starpos + dist, transform.position.y, transform.position.z);
+		if (temp > starpos + length) starpos += length;
+		else if (temp < starpos - length) starpos -= length;
+	}
+}
