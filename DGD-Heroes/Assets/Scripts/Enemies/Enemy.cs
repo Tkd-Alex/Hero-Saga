5ee53c736af90972c8313e4fecdaf9a468edd3a1
Alessandro Maggio <alex.tkd.alex@gmail.com>
2020-04-13 23:35:33 +0200

    Enemy follow the player

--- DGD-Heroes/Assets/Scripts/Enemies/Enemy.cs
+++ DGD-Heroes/Assets/Scripts/Enemies/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,22 +7,24 @@ public class Enemy : MonoBehaviour {
 
 	[SerializeField] GameObject player;
 	[SerializeField] float health = 100;
-	[SerializeField] float speed = 0.4f;
+	[SerializeField] float speed = 0.6f;
+	
+	Rigidbody2D currentRigidBody;
 	public float damage = 5;
+	
 	void Start () {
-		
+		currentRigidBody = gameObject.GetComponent<Rigidbody2D>();
 	}
 
 	void LateUpdate() {
 		// 'Watch' the player, flip enemy. | Little movement
-		if (Vector2.Distance(gameObject.transform.position, player.transform.position) <= 4) {
+		if ((Math.Abs(player.transform.position.x - gameObject.transform.position.x) >= 0.05) &&  Vector2.Distance(gameObject.transform.position, player.transform.position) <= 4) {
 			if (
 				(player.transform.position.x > gameObject.transform.position.x && gameObject.transform.localScale.x > 0) ||
 				(player.transform.position.x < gameObject.transform.position.x && gameObject.transform.localScale.x < 0)
-			)
-				gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
-
-			transform.Translate(Vector2.up * speed * Time.deltaTime);
+			) gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+			
+			currentRigidBody.velocity = new Vector2(gameObject.transform.localScale.x < 0 ? speed : -speed, currentRigidBody.velocity.y);
 		}
 	}
 
